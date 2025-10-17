66184f68e07deb1982db2b5f3380cb16344886cd
Mert Şakar <mertsakar@hotmail.com>
2025-10-17 22:17:36 +0300

    add GetMovie and AddMovie controller functions and routes

--- Server/MagicStreamMoviesServer/controllers/movie_controller.go
+++ Server/MagicStreamMoviesServer/controllers/movie_controller.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"github.com/horzu/MagicStreamMovies/Server/MagicStreamMoviesServer/database"
 	"github.com/horzu/MagicStreamMovies/Server/MagicStreamMoviesServer/models"
 	"go.mongodb.org/mongo-driver/v2/bson"
@@ -13,10 +14,11 @@ import (
 )
 
 var movieCollection *mongo.Collection = database.OpenCollection("movies")
+var validate = validator.New()
 
 func GetMovies() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
 		defer cancel()
 
 		var movies []models.Movie
@@ -24,13 +26,61 @@ func GetMovies() gin.HandlerFunc {
 		cursor, err := movieCollection.Find(ctx, bson.M{})
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error fetching movies from database"})
+			return
 		}
 		defer cursor.Close(ctx)
 
 		if err = cursor.All(ctx, &movies); err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error decoding movies"})
+			return
 		}
 
 		c.JSON(http.StatusOK, movies)
+	}
+}
+
+func GetMovie() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		movieID := c.Param("imdb_id")
+		if movieID == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Movie ID is required"})
+			return
+		}
+
+		var movie models.Movie
+		err := movieCollection.FindOne(ctx, bson.M{"imdb_id": movieID}).Decode(&movie)
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Movie not found"})
+			return
+		}
+		c.JSON(http.StatusOK, movie)
+	}
+}
+
+func AddMovie() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var movie models.Movie
+		if err := c.ShouldBindJSON(&movie); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request payload"})
+			return
+		}
+
+		if err := validate.Struct(movie); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "validation failed" , "details" : err.Error()})
+			return
+		}
+
+		result, err := movieCollection.InsertOne(ctx, movie)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error inserting movie into database"})
+			return
+		}
+		c.JSON(http.StatusCreated, result)
 	}
 }
