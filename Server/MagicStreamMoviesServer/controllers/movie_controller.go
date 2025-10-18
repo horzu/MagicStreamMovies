bdd773512b49a6efd72d801e3d3c68f99ce3b907
Mert Şakar <mertsakar@hotmail.com>
2025-10-18 18:21:46 +0300

    update movie review via AI

--- Server/MagicStreamMoviesServer/controllers/movie_controller.go
+++ Server/MagicStreamMoviesServer/controllers/movie_controller.go
@@ -2,18 +2,25 @@ package controllers
 
 import (
 	"context"
+	"errors"
+	"log"
 	"net/http"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"github.com/horzu/MagicStreamMovies/Server/MagicStreamMoviesServer/database"
 	"github.com/horzu/MagicStreamMovies/Server/MagicStreamMoviesServer/models"
+	"github.com/joho/godotenv"
+	"github.com/tmc/langchaingo/llms/openai"
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 var movieCollection *mongo.Collection = database.OpenCollection("movies")
+var rankingCollection *mongo.Collection = database.OpenCollection("rankings")
 var validate = validator.New()
 
 func GetMovies() gin.HandlerFunc {
@@ -72,7 +79,7 @@ func AddMovie() gin.HandlerFunc {
 		}
 
 		if err := validate.Struct(movie); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "validation failed" , "details" : err.Error()})
+			c.JSON(http.StatusBadRequest, gin.H{"error": "validation failed", "details": err.Error()})
 			return
 		}
 
@@ -83,4 +90,137 @@ func AddMovie() gin.HandlerFunc {
 		}
 		c.JSON(http.StatusCreated, result)
 	}
+}
+
+func AdminReviewUpdate() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		movieId := c.Param("imdb_id")
+		if movieId == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "movieId required"})
+			return
+		}
+
+		var req struct {
+			AdminReview string `json:"admin_review"`
+		}
+		var resp struct {
+			RankingName string `json:"ranking_name"`
+			AdminReview string `json:"admin_review"`
+		}
+
+		if err := c.ShouldBind(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+			return
+		}
+
+		sentiment, rankVal, err := GetReviewRanking(req.AdminReview)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error getting review ranking"})
+			return
+		}
+
+		filter := bson.M{"imdb_id": movieId}
+
+		update := bson.M{
+			"$set": bson.M{
+				"admin_review": req.AdminReview,
+				"ranking": bson.M{
+					"ranking_value": rankVal,
+					"ranking_name":  sentiment,
+				},
+			},
+		}
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		result, err := movieCollection.UpdateOne(ctx, filter, update)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error updating movie"})
+			return
+		}
+
+		if result.MatchedCount == 0 {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Movie not found"})
+			return
+		}
+
+		resp.RankingName = sentiment
+		resp.AdminReview = req.AdminReview
+
+		c.JSON(http.StatusOK, resp)
+
+	}
+}
+
+func GetReviewRanking(admin_review string) (string, int, error) {
+	rankings, err := GetRankings()
+	if err != nil {
+		return "", 0, err
+	}
+
+	sentimentDelimited := ""
+
+	for _, ranking := range rankings {
+		if ranking.RankingValue != 999 {
+			sentimentDelimited = sentimentDelimited + ranking.RankingName + ","
+		}
+	}
+	sentimentDelimited = strings.Trim(sentimentDelimited, ",")
+
+	err = godotenv.Load(".env")
+	if err != nil {
+		log.Println("Warning: .env file not found")
+	}
+
+	OpenRouterAiApiKey := os.Getenv("OPENROUTER_API_KEY")
+	if OpenRouterAiApiKey == "" {
+		return "", 0, errors.New("could not read OPENROUTER_API_KEY")
+	}
+
+	llm, err := openai.New(openai.WithToken(OpenRouterAiApiKey))
+	if err != nil {
+		return "", 0, err
+	}
+
+	basePromptTemplate := os.Getenv("BASE_PROMPT_TEMPLATE")
+	if basePromptTemplate == "" {
+		return "", 0, errors.New("could not read BASE_PROMPT_TEMPLATE")
+	}
+
+	basePrompt := strings.Replace(basePromptTemplate, "{rankings}", sentimentDelimited, 1)
+
+	response, err := llm.Call(context.Background(), basePrompt+admin_review)
+	if err != nil {
+		return "", 0, err
+	}
+
+	rankVal := 0
+
+	for _, ranking := range rankings {
+		if ranking.RankingName == response {
+			rankVal = ranking.RankingValue
+			break
+		}
+	}
+	return response, rankVal, nil
+
+}
+
+func GetRankings() ([]models.Ranking, error) {
+	var rankings []models.Ranking
+
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+
+	cursor, err := rankingCollection.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	if err := cursor.All(ctx, &rankings); err != nil {
+		return nil, err
+	}
+
+	return rankings, nil
 }
