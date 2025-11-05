2bfb6b54e790663d983ee6062b23dd8e8312c260
horzu <mertsakar@hotmail.com>
2025-11-05 22:53:49 +0300

    add header component ui

--- Client/magic-stream-client/src/App.jsx
+++ Client/magic-stream-client/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Home from './components/home/Home'
+import Header from './components/header/Header'
 
 function App() {
 
   return (
     <>
+		<Header />
 		<Home />
     </>
   )
